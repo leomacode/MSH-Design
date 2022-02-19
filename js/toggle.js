3dcb5fe6dd835f304b2787fe1eb53b7c32af1c88
leomacode <forza.leoma@gmail.com>
2022-02-19 18:16:59 +0100

    add customized mobile navbar

---
+++ js/toggle.js
@@ -0,0 +1,26 @@
+const nav_icon = document.getElementsByClassName("nav-icon")[0];
+const nav_items = document.getElementsByClassName("navItems")[0];
+const linkGroup = document.getElementsByClassName("links");
+
+nav_icon.addEventListener("click", () => {
+  nav_items.classList.contains("open")
+    ? nav_items.classList.remove("open")
+    : nav_items.classList.add("open");
+});
+
+[...linkGroup].map((link) => {
+  link.addEventListener("click", () => nav_items.classList.remove("open"));
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !event.target.closest("#nav") &&
+    !event.target.closest("#nav-icon") &&
+    nav_items.classList.contains("open") &&
+    !event.target.type
+  ) {
+    const cl = event.target.closest("#nav-icon");
+    console.log(cl);
+    nav_items.classList.remove("open");
+  }
+});
